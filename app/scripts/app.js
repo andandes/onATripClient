87f2cc411bef9c1447950b00317d3652330e5987
andandes <daren@seznam.cz>
2016-02-03 22:24:47 +0100

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name onAtripApp
+ * @description
+ * # onAtripApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('onAtripApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+	'restangular',
+	'ui.router'
+  ])
+  .config(function ($stateProvider,RestangularProvider) {
+      RestangularProvider.setBaseUrl('http://localhost/TripAdv/api');
+		$stateProvider
+		  .state('home', {
+			url: '/',
+			templateUrl: 'views/main.html',
+			controller: function ($scope) {
+      }
+		  }).state('about', {
+			url: '/about',
+			templateUrl: 'views/about.html',
+			controller: function ($scope) {
+      }
+		  }).state('login', {
+			url: '/login',
+			templateUrl: 'views/login.html',
+			controller: 'LoginCtrl'
+		  });
+	})
+  .controller('MainCtrl', function($scope, Restangular) {
+  // ...
+}).controller('LoginCtrl', function($scope, Restangular) {
+  $scope.loginMethod = function () { 
+      var loginObj = {
+          UserName: $scope.UserName,
+          Password: $scope.Password
+      };
+      var login = Restangular.one("users/login").customPOST({ UserName: "david", Password: "david" }).then(function (data) {
+          alert(data.Token);
+      });;
+
+
+      
+
+  }
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onAtripApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the onAtripApp
+ */
+angular.module('onAtripApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
